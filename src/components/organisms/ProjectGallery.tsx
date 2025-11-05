1ca6dc6d1bc6b70cee2c399aae8438e52310cd04
bryanvillamil <bryanvillamilacevedo@gmail.com>
2025-11-04 22:42:21 -0500

    center box projects

--- src/components/organisms/ProjectGallery.tsx
+++ src/components/organisms/ProjectGallery.tsx
@@ -31,7 +31,7 @@ export default function ProjectGallery({ images, title }: ProjectGalleryProps) {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 auto-rows-[280px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 auto-rows-[280px] m-auto">
         {images.map((url, index) => (
           <div
             key={index}
